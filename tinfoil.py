3c6b83cf606ed88bfa3c6bddea7976f549ce6f10
rmcl15 <ronan06@gmail.com>
2016-03-25 23:07:56 +0000

    Added files via upload
    You will need crocodile clips and tin foil for this project,

---
+++ tinfoil.py
@@ -0,0 +1,13 @@
+from microbit import *
+while True:
+    if pin0.is_touched():
+        display.show(Image.HAPPY)
+    elif pin1.is_touched():
+        display.show(Image.GIRAFFE) 
+    else:
+        display.show(Image.SAD) 
+        display.clear()
+     
+     # print(pin0.is_touched())
+       #sleep(1000)
+# Write your code here :-)
